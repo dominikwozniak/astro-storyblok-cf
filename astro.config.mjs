6883f81449700ca6d7d9ea6e7946cf9f0654e52c
dominik.wozniak <dominik.wozniak@airhelp.com>
2025-05-30 13:44:27 +0200

    Cf pages + initial SSR setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,49 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import { storyblok } from "@storyblok/astro";
+import { loadEnv } from "vite";
+import mkcert from "vite-plugin-mkcert";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
 
-// https://astro.build/config
-export default defineConfig({});
+import cloudflare from "@astrojs/cloudflare";
+
+const env = loadEnv("", process.cwd(), "STORYBLOK");
+
+export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_ACCESS_TOKEN,
+      livePreview: process.env.PUBLIC_ENV !== "production",
+      // bridge: true, // for preview
+      bridge: process.env.PUBLIC_ENV !== "production",
+      // bridge: false,
+      components: {
+        page: "storyblok/Page",
+        feature: "storyblok/Feature",
+        grid: "storyblok/Grid",
+        teaser: "storyblok/Teaser",
+      },
+      apiOptions: {
+        rateLimit: 100,
+      },
+    }),
+    react(),
+  ],
+
+  vite: {
+    plugins: [tailwindcss(), mkcert()],
+    resolve: {
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      alias: import.meta.env.PROD && {
+        "react-dom/server": "react-dom/server.edge",
+      },
+    },
+  },
+
+  output: "server",
+  adapter: cloudflare(),
+  // in ideal case serve only server output for preview to have hot reload
+  // output: process.env.PUBLIC_ENV === 'preview' ? 'server' : 'static',
+  // adapter: process.env.PUBLIC_ENV === 'preview' ? cloudflare() : undefined,
+});
